06ee0333b6d7bc92ae2af3c29e891422dfae84a5
Lasse Krogh Mammen <lkm@bookboon.com>
2017-02-16 17:06:05 +0000

    Get frontpage by slug

--- src/Entity/Frontpage.php
+++ src/Entity/Frontpage.php
@@ -3,6 +3,7 @@
 namespace Bookboon\Api\Entity;
 
 use Bookboon\Api\Bookboon;
+use Bookboon\Api\Exception\UsageException;
 
 class Frontpage extends Entity
 {
@@ -12,14 +13,33 @@ class Frontpage extends Entity
     const SLUG_HIGHEST_RATED = 'highest-rated';
 
     /**
-     * Get Frontpages.
+     * Get all front page books
      *
      * @param Bookboon $bookboon
-     * @return Frontpage|bool
+     * @return Frontpage[]
      */
     public static function get(Bookboon $bookboon)
     {
         return Frontpage::getEntitiesFromArray($bookboon->rawRequest("/frontpage"));
+    }
+
+    /**
+     * Get specific frontpage
+     *
+     * @param Bookboon $bookboon
+     * @param $slug
+     * @return Frontpage
+     * @throws UsageException
+     */
+    public static function getBySlug(Bookboon $bookboon, $slug)
+    {
+        foreach (self::get($bookboon) as $frontpage) {
+            if ($frontpage->getSlug() === $slug) {
+                return $frontpage;
+            }
+        }
+
+        throw new UsageException("Non-existing slug");
     }
 
     protected function isValid(array $array)
